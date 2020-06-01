39f45fbe947fbaa03ca11b357dbe61f655cfb4f5
ziyonghong <1131002466@qq.com>
2020-06-01 23:06:32 +0800

    add "otp验证码获取"

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -14,12 +14,34 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 @Controller("user")
 @RequestMapping("/user")
 public class UserController extends BaseController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    //用户获取otp短信接口
+    @RequestMapping("/getotp")
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name="telphone")String telphone){
+        //需要按照一定的规制生成OTP验证码
+        Random random=new Random();
+        int randomInt=random.nextInt(99999);
+        randomInt+=10000;
+        String otpCode=String.valueOf(randomInt);
+        //将OTP验证码同对应用户的手机号关联(使用HttpSession的方式绑定他的手机号与OTPCODE)
+        httpServletRequest.getSession().setAttribute(telphone,otpCode);
+
+        //将OTP验证码通过短信通道发送给用户，（先不做）
+        System.out.println("telphone: "+telphone+" &otpCode: "+otpCode);
+
+        return CommonReturnType.create(null);
+    }
 
     @RequestMapping("/get")
     @ResponseBody
